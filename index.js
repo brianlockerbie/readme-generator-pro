845e073dc23a2f992643d5433553b79cee64203b
Brian <brian.lockerbie@gmail.com>
2022-04-26 16:46:34 -0700

    added inquirer & question

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter your project name.",
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log("Please entera project name!");
+                return false;
+            }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`./dist/${fileName}.md`, generateMarkdown(data), err => {
+        console.log(err);
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+init = () => {
+    return inquirer
+        .prompt(questions)
+        .then(readMeData => {
+            const { title } = readMeData
+            writeToFile(title, readMeData);
+        })
+        .then(markdownData => {
+            console.log(markdownData);
+        })
+
+}
 
 // Function call to initialize app
 init();
